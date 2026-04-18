b26f48250c3a30ce69affb72f989edc1c73707c9
Abdelrhman Atta <abdelrhmatta@gmail.com>
2026-04-18 09:27:44 +0200

    added ./tests/io_tests.cpp

---
+++ tests/io_tests.cpp
@@ -0,0 +1,40 @@
+#include "io.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdint>
+
+struct rawImage_t
+{
+    std::string fileName;
+    std::string destName;
+    uint32_t imageWidth;
+    uint32_t imageHeight;
+};
+
+int main()
+{
+    const uint32_t dim = 512;
+    std::vector<rawImage_t> images = 
+    {
+        {"rect.raw", "rect_copy.raw", dim, dim},
+        {"circ.raw", "circ_copy.raw", dim, dim},
+        {"diag.raw", "diag_copy.raw", dim, dim},
+    };
+
+    for(const auto& [src, dest, w, h] : images)
+    {
+        uint8_t* imageBuffer = IO_LoadRaw(src, w, h);
+        if (imageBuffer != nullptr) 
+        {
+            IO_SaveRaw(dest, imageBuffer, w, h);
+            std::free(imageBuffer);
+        }
+        else 
+        {
+            std::cerr << "Skipping save for " << src << " due to load failure." << std::endl;
+        }
+    }
+
+    return 0;
+}
